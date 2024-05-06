6ba4ebc9a4d180cd3d2df0a997b170e5c739dd19
romuloff22 <romuloffranco@gmail.com>
2024-05-06 10:22:31 -0300

    teste vpc

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,18 @@
-output "subnet_id" {
-  description = "Id da Subnet"
-  value       = aws_subnet.subnet-homo.id
+output "subnet01_id" {
+  description = "Id da Subnet 01"
+  value       = aws_subnet.subnet01-homo.id
 }
+
+output "subnet02_id" {
+  description = "Id da Subnet 02"
+  value       = aws_subnet.subnet02-homo.id
+}
+
+output "subnet03_id" {
+  description = "Id da Subnet 03"
+  value       = aws_subnet.subnet03-homo.id
+}
+
 
 output "security_group_id" {
   description = "ID do Security Group"
