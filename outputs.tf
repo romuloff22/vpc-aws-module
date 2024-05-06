dd504b7d8f7b7750b620d2ac6bcb3826468b6cae
romuloff22 <romuloffranco@gmail.com>
2024-05-06 15:56:49 -0300

    Teste network 2

--- outputs.tf
+++ outputs.tf
@@ -28,7 +28,27 @@ output "subnet6_id" {
   value       = aws_subnet.subnet6-homo.id
 }
 
-output "security_group_id" {
+output "security_group01_id" {
   description = "ID do Security Group"
-  value       = aws_security_group.security_group.id
+  value       = aws_security_group.security_group_01.id
+}
+
+output "security_group02_id" {
+  description = "ID do Security Group"
+  value       = aws_security_group.security_group_02.id
+}
+
+output "security_group03_id" {
+  description = "ID do Security Group"
+  value       = aws_security_group.security_group_03.id
+}
+
+output "security_group04_id" {
+  description = "ID do Security Group"
+  value       = aws_security_group.security_group_04.id
+}
+
+output "security_group05_id" {
+  description = "ID do Security Group"
+  value       = aws_security_group.seurity_group_05.id
 }
