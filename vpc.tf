bbe146f3737382b2fb69b3e574ec04b0b183102f
romuloff22 <romuloffranco@gmail.com>
2024-05-08 09:52:49 -0300

    TESTE001

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "vpc-homo" {
   }
 }
 
-resource "aws_subnet" "subnet01" {
+/*resource "aws_subnet" "subnet01" {
   vpc_id     = aws_vpc.vpc-homo.id
   cidr_block = var.cidr_subnet
   availability_zone = "us-east-2a"
@@ -24,7 +24,7 @@ resource "aws_subnet" "subnet02" {
   tags = {
     Name = "subnet02-${var.environment}"
   }
-}
+}*/
 
 resource "aws_subnet" "subnet03" {
   vpc_id     = aws_vpc.vpc-homo.id
@@ -88,7 +88,7 @@ resource "aws_route_table" "route_table" {
   }
 }
 
-resource "aws_route_table_association" "rta1" {
+/*resource "aws_route_table_association" "rta1" {
   subnet_id      = aws_subnet.subnet01.id
   route_table_id = aws_route_table.route_table.id
 }
@@ -96,14 +96,14 @@ resource "aws_route_table_association" "rta1" {
 resource "aws_route_table_association" "rta2" {
   subnet_id      = aws_subnet.subnet02.id
   route_table_id = aws_route_table.route_table.id
-}
+}*/
 
 resource "aws_route_table_association" "rta3" {
   subnet_id      = aws_subnet.subnet03.id
   route_table_id = aws_route_table.route_table.id
 }
 
-resource "aws_security_group" "security_group_01" {
+/*resource "aws_security_group" "security_group_01" {
   name        = "launch-wizard-1-${var.environment}"
   description = "Permitir acesso a porta 22, 80 e 443"
   vpc_id      = aws_vpc.vpc-homo.id
@@ -141,7 +141,7 @@ resource "aws_security_group" "security_group_01" {
   tags = {
     Name = "security-group-${var.environment}"
   }
-}
+}*/
 
 resource "aws_security_group" "security_group_02" {
   name        = "launch-wizard-2-${var.environment}"
@@ -247,7 +247,7 @@ resource "aws_security_group" "security_group_02" {
   }
 }
 
-resource "aws_security_group" "security_group_03" {
+/*resource "aws_security_group" "security_group_03" {
   name        = "launch-wizard-3-${var.environment}"
   description = "Permitir acesso a porta 22"
   vpc_id      = aws_vpc.vpc-homo.id
@@ -277,7 +277,7 @@ resource "aws_security_group" "security_group_04" {
     protocol    = "tcp"
     cidr_blocks = ["186.206.254.117/32", "191.34.39.253/32", "172.31.29.21/32"]
   }
-}
+}*/
 
 resource "aws_security_group" "seurity_group_05" {
   name        = "rds-launch-wizard-${var.environment}"
@@ -302,5 +302,14 @@ resource "aws_security_group" "seurity_group_05" {
 
   tags = {
     Name = "security-group-${var.environment}"
+  }
+}
+
+resource "aws_db_subnet_group" "subnet_group_rds" {
+  description       = "Created from the RDS Homolog Management Console"
+  subnet_ids        = [aws_subnet.subnet04.id, aws_subnet.subnet05.id,aws_subnet.subnet06.id]
+
+  tags = {
+    Name = "db-subnet-group-name-${var.environment}"
   }
 }
